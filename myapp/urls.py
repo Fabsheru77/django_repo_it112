e7b141560d40f6c3452aee86ac042d1fdb3070f5
Faris MA <92819860+Fabsheru77@users.noreply.github.com>
2024-03-06 02:55:21 +0000

    Added Class and Function to Django repo

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,7 +1,11 @@
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
   path("",views.home, name="home"),
-  path("about",views.about, name="about"),
+  path("about/",views.about, name="about"),
+  path('function/', views.function_view, name='function_view'),
+  path('class/', views.ClassView.as_view(), name='class_view'),
 ]
+
