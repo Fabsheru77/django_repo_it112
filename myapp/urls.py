ad46316eb38a723d38e9f5bbd05f5196934a54e7
Faris MA <92819860+Fabsheru77@users.noreply.github.com>
2024-03-08 03:44:56 +0000

    Bootswatch Base Template updated and 4 Web pages created

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('home/', views.Home_view, name='home_view'),
+  path('', views.Home_view, name='home_view'),
   path('about/', views.About_Page_view, name='about_view'),
   path('function/', views.function_view, name='function_view'),
   path('class/', views.ClassView.as_view(), name='class_view'),
