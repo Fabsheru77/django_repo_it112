3dbebad6d2a9f35d423fdfef95d2d27f0e24c5da
Faris MA <92819860+Fabsheru77@users.noreply.github.com>
2024-03-16 01:31:58 +0000

    Adds Django administration Login

--- django_project/settings.py
+++ django_project/settings.py
@@ -122,3 +122,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# settings.py
+LOGIN_URL = 'admin:login'  # Redirect to the admin login page
+
